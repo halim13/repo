f4a5d98d2a405e6f40c648a0e56c4c2c237cb778
halim13 <hasanudinhalim@gmail.com>
2022-05-19 07:33:40 +0700

    first blood

---
+++ src/components/Item.jsx
@@ -0,0 +1,7 @@
+const TodoItem = () => {
+    return ( 
+        <li>Hit the gym <span className="close">x</span></li>
+    )
+}
+ 
+export default TodoItem
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,13 @@
+import Item from './Item'
+
+const ItemList = () => {
+    return ( 
+        <ul id="todo-list">
+            <Item />
+            <Item />
+            <Item />
+        </ul>
+    );
+}
+ 
+export default ItemList;
