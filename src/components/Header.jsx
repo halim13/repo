835dd12acc86758e184690b21318f6802ed1ed42
halim13 <hasanudinhalim@gmail.com>
2022-05-21 00:58:48 +0700

    adjust: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-const Header = () => {
+const Header = ({ onClick, setSearch }) => {
     return ( 
     <div id="todo-header" className="header">
         <h1>List Repository Github</h1>	
-        <input type="text" placeholder="Search Username..." />
-        <span className="add-button">Get List</span>
+        <input type="text" placeholder="Search Username..." onChange={(e) => setSearch(e.target.value)} onKeyDown={e => e.key === 'Enter' ? onClick() : null} />
+        <span className="add-button" onClick={onClick}>Get List</span>
     </div>
 )}
 
